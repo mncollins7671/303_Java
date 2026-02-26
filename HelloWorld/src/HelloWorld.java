5d6f0fc058250b7c7d0fd5a7716884829c15eb40
mncollins7671 <matthew.neil.collins@gmail.com>
2026-02-26 09:52:24 -0500

    lesson 1

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My name is Matt.");
+        System.out.println("I'm 31 years old.");
+    }
+}
