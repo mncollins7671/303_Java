818c98b6f371e065a2a4f25df7a1e87177bfbe4d
mncollins7671 <matthew.neil.collins@gmail.com>
2026-03-13 16:05:36 -0400

    update

---
+++ lessons/lesson-02-variables-types-operators/java-code/com/perscholas/java_basics/basics.java
@@ -0,0 +1,32 @@
+package com.perscholas.java_basics;
+
+public class basics {
+    public static void main(String[] args) {
+
+        //Part 1
+        int v1 = 6;
+        int v2 = 3;
+        int sum = v1 + v2;
+        System.out.println(sum);
+
+        //Part 2
+        double d1 = 2.4;
+        double d2 = 9.7;
+        double sum2 = d1 + d2;
+        System.out.println(sum2);
+
+        //Part 3
+        int num = 3;
+        double dec = 4.7;
+        double sum3 = num + dec;
+        System.out.println(sum3);
+
+        //Part 4
+        int a = 58;
+        int b = 61;
+
+
+        //Part 5
+
+    }
+}
